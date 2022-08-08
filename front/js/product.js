a282523125c35762d892eefd1007709b56977de0
ortic01 <camel762000@gmail.com>
2022-08-08 15:10:29 +0200

    avancement de la page product

--- front/js/product.js
+++ front/js/product.js
@@ -3,6 +3,10 @@ const params = new URLSearchParams(location.search);
 const id = params.get("id")
 console.log(id)
 
+let product = null
+let select = document.getElementById("colors")
+let itemQuantity = document.getElementById("quantity")
+
 //affichage des information du canapé
 function information(canape){
     //affichage de l'image
@@ -25,12 +29,12 @@ function information(canape){
     p.textContent = canape.description
 
     //affichage des couleurs du canapé
-    let select = document.getElementById("colors")
+    
     for(let couleur of canape.colors){
-        let option = document.createElement("option")
-        option.textContent = couleur
-        option.value = couleur
-        select.appendChild(option)
+        let option = document.createElement("option");
+        option.textContent = couleur;
+        option.value = couleur;
+        select.appendChild(option);
     }
 
 }
@@ -40,6 +44,7 @@ fetch("http://localhost:3000/api/products/"+id).then(res => res.json()).then(can
 
    information(canape)
    console.log(canape)
+   product = canape
 })
 
 //paramettrage boutton validation
@@ -49,9 +54,43 @@ function validation() {
 
     //clic sur le boutton
     button.addEventListener("click",function(){
-        console.log("clic sur boutton")
+        // console.log("clic sur boutton")
+        ajoutPanier()
+
+      
     })
 }
+// ajout dans le localstorage
+function ajoutPanier() {
+    let color = select.value
+    if(!color) {
+        alert("selectionner une couleur")
+        return ;
+    }
+    let quantity = itemQuantity.value
+    if(quantity < 1 || quantity > 100 ) {
+        alert("selectionner un nombre entre 1 et 100")
+        return ;
+    }
+    let element = {id: product._id, color: color, quantity: quantity}
+    let panier = getPanier()
+    panier.push(element)
+    localStorage.setItem("panier", JSON.stringify(panier))
+}
 
+function getPanier() {
+    let panier = localStorage.getItem("panier")
+    if(panier == null) {
+        return []
+    
+    } else {
+        return JSON.parse(panier)
+    }
+    
+}
 validation()
+
+
+
+
 
